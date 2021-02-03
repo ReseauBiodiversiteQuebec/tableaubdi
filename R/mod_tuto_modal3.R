7e5098d49e12ceae2bfb185b22c603dbac33654d
Vincent Bellavance <Vincent.Bellavance@Usherbrooke.ca>
2021-02-03 11:59:30 -0500

    fix modal output dir strike 2

--- R/mod_tuto_modal3.R
+++ R/mod_tuto_modal3.R
@@ -24,7 +24,7 @@ mod_tuto_modal3_server <- function(id){
     showModal(
       modalDialog(
         includeHTML(
-          rmarkdown::render("data-raw/thirdModal.Rmd", output_dir = "/data", quiet = TRUE)
+          rmarkdown::render("data-raw/thirdModal.Rmd", output_dir = "data", quiet = TRUE)
         ),
         actionButton("qc", "Tout le Québec"),
         actionButton("pro_nat", "Par provinces naturelles"),
